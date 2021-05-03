9ad1bb965a664d2bdbd6b0703a0224bcb931b98e
Sidharth <sidharthram360@gmail.com>
2021-05-03 12:34:05 +0530

    backend token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Auth from './components/Auth';
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -10,11 +10,34 @@ function SignUp() {
 
     const signUp = () => {
         auth.createUserWithEmailAndPassword(email, password).then(res => {
+            postCred()
             history.push('/main');
             console.log('SignedUp')
+
             //do something with the response
         }).catch(err => console.log(err))
     }
+
+    function postCred() {
+        fetch('http://localhost:8000/users/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: email,
+              password:password
+            })
+            
+          })
+            .then(res => console.log(res))
+            .then(json => {
+              localStorage.setItem('token', json.token);
+              
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className='signUp'>
             <h1>Register your account</h1>
